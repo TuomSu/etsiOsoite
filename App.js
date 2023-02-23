351d7c1564eeb1956c9e6210a1f85a2833408414
Susanna <susanna.tuomi@myy.haaga-helia.fi>
2023-02-23 11:03:18 +0200

    Etsi osoite ja sijainti

--- App.js
+++ App.js
@@ -1,14 +1,96 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import MapView, {Marker} from 'react-native-maps';
+import * as Location from 'expo-location';
+
 
 export default function App() {
+
+  const [address, setAddress] = useState('');
+  const [coordinates, setCoordinates] = useState({
+    latitude: 0, 
+    longitude: 0, 
+    latitudeDelta: 0.0322,
+    longitudeDelta: 0.0221});
+  
+  
+
+  useEffect(() => {
+      (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('No permission to get location')
+        return;
+      }
+      let locate = await Location.getCurrentPositionAsync({});
+      setCoordinates({
+        latitude: parseFloat(locate.coords.latitude), 
+        longitude: parseFloat(locate.coords.longitude), 
+        latitudeDelta: 0.0322,
+        longitudeDelta: 0.0221});
+      console.log('Location:', coordinates);
+      
+  })();
+    }, []);
+
+
+
+    const getPosition = async () => {
+      console.log({address})
+      try{
+      const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=API_KEY`)
+      const data = await response.json();
+  
+      console.log(data);
+      setCoordinates(
+        {latitude: data.results[0].geometry.location.lat,
+        longitude: data.results[0].geometry.location.lng,
+        latitudeDelta: 0.0322,
+        longitudeDelta: 0.0221})
+      
+      }
+      
+    catch (e){
+      Alert.alert('Error fetching data');
+    }
+    
+
+      console.log({coordinates})
+      
+  }
+  
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <MapView
+        style={styles.map}
+        region={coordinates}
+        
+      >
+        <Marker
+          coordinate={coordinates}
+          //title= {address}
+        />
+        <Marker
+          coordinate={coordinates}
+          title= {address}
+        />
+        
+      </MapView>
+      
+      <TextInput style={{height: 40, width:200 }}  
+      placeholder='insert address'
+      onChangeText={text => setAddress(text) } 
+      value={address}/>
+      <Button title="Show" onPress= {getPosition} />
+      <StatusBar style="hidden" />
     </View>
   );
-}
+
+  }
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +99,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    flex: 1,
+    width: "100%",
+    height: "75%"
+  }
 });
+
